d75c5d576a89366971fc5861b1d600b939ed23ca
Abu jafar sisty <124616816+Sisty37@users.noreply.github.com>
2024-09-27 21:58:17 +0600

    Add files via upload

---
+++ PET_HOSTEL/PaymentMethod.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace PET_HOSTEL
+{
+    partial class PaymentMethod
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaymentMethod));
+            this.label1 = new System.Windows.Forms.Label();
+            this.visa = new System.Windows.Forms.PictureBox();
+            this.masterCard = new System.Windows.Forms.PictureBox();
+            this.payPal = new System.Windows.Forms.PictureBox();
+            this.bkash = new System.Windows.Forms.PictureBox();
+            this.nogod = new System.Windows.Forms.PictureBox();
+            this.rocked = new System.Windows.Forms.PictureBox();
+            this.upay = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.visa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.payPal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bkash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nogod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocked)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upay)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(341, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(520, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pay Your Amount To Confirm Booking";
+            // 
+            // visa
+            // 
+            this.visa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.visa.Image = ((System.Drawing.Image)(resources.GetObject("visa.Image")));
+            this.visa.Location = new System.Drawing.Point(195, 191);
+            this.visa.Name = "visa";
+            this.visa.Size = new System.Drawing.Size(201, 103);
+            this.visa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.visa.TabIndex = 1;
+            this.visa.TabStop = false;
+            this.visa.Click += new System.EventHandler(this.visa_Click);
+            // 
+            // masterCard
+            // 
+            this.masterCard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.masterCard.Image = ((System.Drawing.Image)(resources.GetObject("masterCard.Image")));
+            this.masterCard.Location = new System.Drawing.Point(454, 191);
+            this.masterCard.Name = "masterCard";
+            this.masterCard.Size = new System.Drawing.Size(201, 103);
+            this.masterCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.masterCard.TabIndex = 2;
+            this.masterCard.TabStop = false;
+            this.masterCard.Click += new System.EventHandler(this.masterCard_Click);
+            // 
+            // payPal
+            // 
+            this.payPal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.payPal.Image = ((System.Drawing.Image)(resources.GetObject("payPal.Image")));
+            this.payPal.Location = new System.Drawing.Point(707, 191);
+            this.payPal.Name = "payPal";
+            this.payPal.Size = new System.Drawing.Size(201, 103);
+            this.payPal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.payPal.TabIndex = 3;
+            this.payPal.TabStop = false;
+            this.payPal.Click += new System.EventHandler(this.payPal_Click);
+            // 
+            // bkash
+            // 
+            this.bkash.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bkash.Image = ((System.Drawing.Image)(resources.GetObject("bkash.Image")));
+            this.bkash.Location = new System.Drawing.Point(194, 336);
+            this.bkash.Name = "bkash";
+            this.bkash.Size = new System.Drawing.Size(185, 103);
+            this.bkash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bkash.TabIndex = 4;
+            this.bkash.TabStop = false;
+            this.bkash.Click += new System.EventHandler(this.bkash_Click);
+            // 
+            // nogod
+            // 
+            this.nogod.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nogod.Image = ((System.Drawing.Image)(resources.GetObject("nogod.Image")));
+            this.nogod.Location = new System.Drawing.Point(411, 336);
+            this.nogod.Name = "nogod";
+            this.nogod.Size = new System.Drawing.Size(173, 103);
+            this.nogod.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.nogod.TabIndex = 5;
+            this.nogod.TabStop = false;
+            this.nogod.Click += new System.EventHandler(this.nogod_Click);
+            // 
+            // rocked
+            // 
+            this.rocked.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rocked.Image = ((System.Drawing.Image)(resources.GetObject("rocked.Image")));
+            this.rocked.Location = new System.Drawing.Point(613, 336);
+            this.rocked.Name = "rocked";
+            this.rocked.Size = new System.Drawing.Size(166, 103);
+            this.rocked.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rocked.TabIndex = 6;
+            this.rocked.TabStop = false;
+            this.rocked.Click += new System.EventHandler(this.rocked_Click);
+            // 
+            // upay
+            // 
+            this.upay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.upay.Image = ((System.Drawing.Image)(resources.GetObject("upay.Image")));
+            this.upay.Location = new System.Drawing.Point(806, 336);
+            this.upay.Name = "upay";
+            this.upay.Size = new System.Drawing.Size(169, 103);
+            this.upay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.upay.TabIndex = 7;
+            this.upay.TabStop = false;
+            this.upay.Click += new System.EventHandler(this.upay_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1165, 74);
+            this.panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select Your Payment Method:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(207, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 23);
+            this.label3.TabIndex = 10;
+            // 
+            // PaymentMethod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1165, 655);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.upay);
+            this.Controls.Add(this.rocked);
+            this.Controls.Add(this.nogod);
+            this.Controls.Add(this.bkash);
+            this.Controls.Add(this.payPal);
+            this.Controls.Add(this.masterCard);
+            this.Controls.Add(this.visa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PaymentMethod";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.visa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.payPal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bkash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nogod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocked)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upay)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox visa;
+        private System.Windows.Forms.PictureBox masterCard;
+        private System.Windows.Forms.PictureBox payPal;
+        private System.Windows.Forms.PictureBox bkash;
+        private System.Windows.Forms.PictureBox nogod;
+        private System.Windows.Forms.PictureBox rocked;
+        private System.Windows.Forms.PictureBox upay;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ PET_HOSTEL/PaymentMethod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PET_HOSTEL
+{
+    public partial class PaymentMethod : Form
+    {
+        public PaymentMethod()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void visa_Click(object sender, EventArgs e)
+        {
+            CardPayment a = new CardPayment();
+            a.Show();         
+        }
+
+        private void masterCard_Click(object sender, EventArgs e)
+        {
+            CardPayment b = new CardPayment();
+            b.Show();         
+        }
+
+        private void payPal_Click(object sender, EventArgs e)
+        {
+            CardPayment c = new CardPayment();
+            c.Show();
+        }
+
+        private void bkash_Click(object sender, EventArgs e)
+        {
+            MobileBankingPayment d = new MobileBankingPayment();
+            d.Show();
+        }
+
+        private void nogod_Click(object sender, EventArgs e)
+        {
+            MobileBankingPayment d = new MobileBankingPayment();
+            d.Show();         
+        }
+
+        private void rocked_Click(object sender, EventArgs e)
+        {
+            MobileBankingPayment d = new MobileBankingPayment();
+            d.Show();
+        }
+
+        private void upay_Click(object sender, EventArgs e)
+        {
+            MobileBankingPayment d = new MobileBankingPayment();
+            d.Show();
+        }
+    }
+}

a8a286cc32ec19d2ae99b6442b782e9ef94b0436
Md. Istiaq Mozumder <iftistiaq@gmail.com>
2024-09-28 13:20:18 +0600

    Change

--- PET_HOSTEL/MobileBankingPayment.Designer.cs
+++ PET_HOSTEL/MobileBankingPayment.Designer.cs
@@ -64,7 +64,7 @@ namespace PET_HOSTEL
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(126, 293);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.Size = new System.Drawing.Size(29, 16);
             this.label3.TabIndex = 24;
             this.label3.Text = "Pin:";
             // 
@@ -73,7 +73,7 @@ namespace PET_HOSTEL
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(126, 238);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.Size = new System.Drawing.Size(100, 16);
             this.label2.TabIndex = 23;
             this.label2.Text = "Phone Number:";
             // 
@@ -82,7 +82,7 @@ namespace PET_HOSTEL
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(126, 120);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.Size = new System.Drawing.Size(73, 16);
             this.label1.TabIndex = 22;
             this.label1.Text = "Username:";
             // 
@@ -127,7 +127,7 @@ namespace PET_HOSTEL
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(126, 177);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.Size = new System.Drawing.Size(55, 16);
             this.label5.TabIndex = 27;
             this.label5.Text = "Amount:";
             // 
@@ -153,7 +153,7 @@ namespace PET_HOSTEL
             this.button_Print.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Print.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_Print.ForeColor = System.Drawing.Color.White;
-            this.button_Print.Location = new System.Drawing.Point(532, 130);
+            this.button_Print.Location = new System.Drawing.Point(531, 248);
             this.button_Print.Name = "button_Print";
             this.button_Print.Size = new System.Drawing.Size(133, 36);
             this.button_Print.TabIndex = 29;
@@ -165,9 +165,9 @@ namespace PET_HOSTEL
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label6.Location = new System.Drawing.Point(541, 169);
+            this.label6.Location = new System.Drawing.Point(539, 293);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(124, 34);
+            this.label6.Size = new System.Drawing.Size(113, 32);
             this.label6.TabIndex = 30;
             this.label6.Text = "Print receipt to get\r\nyour token";
             this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;

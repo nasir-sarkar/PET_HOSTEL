426703df68e917aaec9e11012c06f21dcc5dcc09
nasir-sarkar <nasirsarkar537@gmail.com>
2024-09-30 19:24:26 +0600

    Query files addition

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE cost
+(
+  cat_cost INT DEFAULT ((0)) NOT NULL,
+  dog_cost INT DEFAULT ((0)) NOT NULL,
+  rabbit_cost INT DEFAULT ((0)) NOT NULL,
+  tortoise_cost INT DEFAULT ((0)) NOT NULL,
+  hamster_cost INT DEFAULT ((0)) NOT NULL,
+  bird_cost INT DEFAULT ((0)) NOT NULL,
+  fish_cost INT DEFAULT ((0)) NOT NULL,
+)
---
+++ SQLQuery3_set-all-initial-value-zero-at-cost-table.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO cost (cat_cost, dog_cost, rabbit_cost, tortoise_cost, hamster_cost, bird_cost, fish_cost)
+VALUES (0, 0, 0, 0, 0, 0, 0);
